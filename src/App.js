ac10a0a7b843def33a94a275e1a14d43b5bc3689
Oksana Repchak <oksa.repchak@gmail.com>
2020-11-26 20:52:57 +0200

    завдання-1

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import user from './components/Profile/user.json';
+import Profile from './components/Profile/Profile';
+
+// import statisticalData from './components/Statistics/statistical-data.json';
+// import Statistics from './components/Statistics/Statistics';
+
+// import friends from './components/FriendList/friends.json';
+// import FriendList from './components/FriendList/FriendList';
+
+
+export default function App() {
+  return (
+    <div>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      {/* <Statistics title="Upload stats" stats={statisticalData} />
+      <Statistics stats={statisticalData} />
+      
+      <FriendList friends={friends} /> */}
+    </div>
+  );
+}
